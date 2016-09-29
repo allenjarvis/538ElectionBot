dbd04a36055314bbe655c5ddba628a49cc8c15e6
Allen Jarvis <allencalebjarvis@gmail.com>
2016-09-29 17:18:01 -0400

    fixes found in testing

--- parseANDpost.py
+++ parseANDpost.py
@@ -1,33 +1,51 @@
 from lxml import html
 import requests
-from twitter.api import Twitter
+from TwitterAPI import TwitterAPI
 
-twitter = Twitter('electionbot538','Th1s1smy9assw0rd')
-current = open("current.txt")
-Hillary = int(current.readline()[0,3])
-Donald = int(current.readline()[0,3])
+api = TwitterAPI('y7uybSTbSXyHkve16gHaAgObI',
+                 'gjSMPeCpletnARVihBl4hlYOTUMNsb5mTaU0pOr6eU89QvK0Lo',
+                 '781536298506985476-nBrmnvlkOf4F6HPEHK6AWOCyyPSLAfO',
+                 'KqOnzmQEfyGOhuqEft4t8XyALEp5XsEw6A40qh8W6l9DU')
+
+current = open("current.txt", 'r')
+Hillary = current.readline()[0:-1]
+Donald = current.readline()[0:-1]
+current.close
+
 print Hillary
 print Donald
 
 page = requests.get('http://projects.fivethirtyeight.com/2016-election-forecast/')
 tree = html.fromstring(page.content)
-HillaryNew = tree.xpath('//*[@id="cardsets"]/div[1]/div/div[4]/div[1]/div/div[1]/div[1]/div[2]/p[2]/text()')
-DonaldNew = tree.xpath('//*[@id="cardsets"]/div[1]/div/div[4]/div[1]/div/div[1]/div[2]/div[1]/p[2]/text()')
+HillaryNew = tree.xpath('//*[@id="cardsets"]/div[1]/div/div[4]/div[1]/div/div[1]/div[1]/div[2]/p[2]/text()')[0]
+DonaldNew = tree.xpath('//*[@id="cardsets"]/div[1]/div/div[4]/div[1]/div/div[1]/div[2]/div[1]/p[2]/text()')[0]
 #check that these are numbers instead of strings?
 
 print HillaryNew
 print DonaldNew
 
-HillaryUP = 'Hillary is UP! Liklihood of a Clinton win went from ' + Hillary + ' to ' + HillaryNew
-DonaldUP = 'Donald is UP. Liklihood of a Donald win went from ' + Donald + ' to ' + DonaldNew
+HillaryUP = 'Hillary is UP! Likelihood of a Clinton win went from ' + Hillary + ' to ' + HillaryNew
+DonaldUP = 'Donald is UP. Likelihood of a Donald win went from ' + Donald + ' to ' + DonaldNew
 
-if HillaryNew >= Hillary:
-  twitter.statuses.update(status=HillaryUP)
-  Hillary = HillaryNew
-  Donald = DonaldNew
-if DonaldNew >= Donald:
-  twitter.statuses.update(status=DonaldUP)
-  Hillary = HillaryNew
-  Donald = DonaldNew
+if float(HillaryNew) > float(Hillary):
+	r = api.request('statuses/update', {'status': HillaryUP})
+	print 'SUCCESS' if r.status_code == 200 else 'FAILURE'
 
-current.close
+	current = open("current.txt", 'w')
+	current.write(HillaryNew + '\n' + DonaldNew + '\n')
+	current.close
+
+
+	Hillary = HillaryNew
+  	Donald = DonaldNew
+if float(DonaldNew) > float(Donald):
+	r = api.request('statuses/update', {'status': DonaldUP})
+	print 'SUCCESS' if r.status_code == 200 else 'FAILURE'
+
+	current = open("current.txt", 'w')
+	current.write(HillaryNew + '\n' + DonaldNew + '\n')
+	current.close
+
+	Hillary = HillaryNew
+  	Donald = DonaldNew
+
