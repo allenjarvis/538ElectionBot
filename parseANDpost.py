6838c9074e001f5ed2a2205236e4463b085fbdd9
Allen Jarvis <allencalebjarvis@gmail.com>
2016-10-05 18:30:48 -0400

    Update parseANDpost.py

--- parseANDpost.py
+++ parseANDpost.py
@@ -74,7 +74,7 @@ def timed_job():
 	#case: tied
 	if DonaldNew==HillaryNew:
 		AllEven = "They're tied, folks. @FiveThirtyEight #Clinton #Trump #538 #election2016"
-		#twitter.update_status(status=AllEven)
+		twitter.update_status(status=AllEven)
 		print AllEven
 	#case: Hillary up
 	elif float(HillaryNew)>float(Hillary):
@@ -84,7 +84,7 @@ def timed_job():
 		else:
 			leadstrails = 'trails'
 		HillaryUP = 'Clinton gained ' + gain + '%! She now ' + leadstrails + ' Donald ' + HillaryNew + '% to ' + DonaldNew + "% in the @FiveThirtyEight polls-only forecast.\r\n#Clinton #Trump #538 #election2016"
-		#twitter.update_status(status=HillaryUP)
+		twitter.update_status(status=HillaryUP)
 		print "POST: " + HillaryUP
 	#case: Donald up
 	elif float(DonaldNew)>float(Donald):
@@ -94,7 +94,7 @@ def timed_job():
 		else:
 			leadstrails = 'trails'
 		DonaldUP = 'Donald gained ' + gain + '%. He now ' + leadstrails + ' Clinton ' + DonaldNew + '% to ' + HillaryNew + "% in the @FiveThirtyEight polls-only forecast.\r\n#Clinton #Trump #538 #election2016"
-		#twitter.update_status(status=DonaldUP)
+		twitter.update_status(status=DonaldUP)
 		print "POST: " + DonaldUP
 	#case: no change
 	else:
